bcca9364cd1ed5f95dd51ab6ced959881c0800f4
Dimitar Zlatev <zlatevbg@gmail.com>
2022-02-20 14:00:10 +0200

    Handle NULL dates

--- app/Http/Resources/AdminResource.php
+++ app/Http/Resources/AdminResource.php
@@ -21,14 +21,14 @@ class AdminResource extends JsonResource
             'last_name' => $this->last_name,
             'is_admin' => $this->is_admin,
             'email' => $this->email,
-            'email_verified_at' => $this->email_verified_at->format('d.m.Y'),
+            'email_verified_at' => $this->email_verified_at ? $this->email_verified_at->format('d.m.Y') : null,
             'avatar' => $this->avatar,
             'address' => $this->address,
             'phone_number' => $this->phone_number,
             'is_marketing' => $this->is_marketing,
-            'created_at' => $this->created_at->format('d.m.Y'),
-            'updated_at' => $this->updated_at->format('d.m.Y'),
-            'last_login_at' => $this->last_login_at->format('d.m.Y'),
+            'created_at' => $this->created_at ? $this->created_at->format('d.m.Y') : null,
+            'updated_at' => $this->updated_at ? $this->updated_at->format('d.m.Y') : null,
+            'last_login_at' => $this->last_login_at ? $this->last_login_at->format('d.m.Y') : null,
         ];
     }
 }
